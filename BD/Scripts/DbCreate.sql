9411e10248365d801ce6ccd671662e0579e32eea
gismiranda <53460005+gismiranda@users.noreply.github.com>
2025-02-02 23:41:28 -0600

    Add files via upload

--- BD/Scripts/DbCreate.sql
+++ BD/Scripts/DbCreate.sql
@@ -1,21 +1,24 @@
 DROP DATABASE IF EXISTS cmsTest;
 CREATE DATABASE cmsTest;
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*Run dotnet ef database update, after create database, then execute zthe table create*/
 
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    tags JSONB,
-    posted_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(id)
-);
+CREATE TABLE IF NOT EXISTS public.posts
+(
+    id integer NOT NULL DEFAULT nextval('posts_postid_seq'::regclass),
+    user_id integer NOT NULL,
+    title character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    content text COLLATE pg_catalog."default" NOT NULL,
+    tags jsonb,
+    posted_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT posts_pkey PRIMARY KEY (id),
+    CONSTRAINT posts_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.posts
+    OWNER to postgres;
--- BD/Scripts/Inserts.sql
+++ BD/Scripts/Inserts.sql
@@ -1,4 +1,4 @@
-TRUNCATE TABLE posts, users RESTART IDENTITY;
+TRUNCATE TABLE posts, AspNetUsers RESTART IDENTITY;
 
 -- Insert 10000 users
 DO $$
@@ -6,8 +6,8 @@ DECLARE
     i INT;
 BEGIN
     FOR i IN 1..10000 LOOP
-        INSERT INTO users (name, email, password_hash)
-        VALUES ('User' || i, 'user' || i || '@example.com', 'AQAAAAIAAYagAAAAEF27Z1uPCCln3huiLOVn7055GuxPYbhhMknaIJC9jZK8v1A96fTcibqSEQ9ATWSVzA==');
+        INSERT INTO AspNetUsers (name,created_at,updated_at,UserName, NormalizedUserName, Email, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled, AccessFailedCount)
+        VALUES ('User' || i,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'user' || i || '@example.com',UPPER('user' || i || '@example.com'),'user' || i || '@example.com',UPPER('user' || i || '@example.com'), FALSE , 'AQAAAAIAAYagAAAAEF27Z1uPCCln3huiLOVn7055GuxPYbhhMknaIJC9jZK8v1A96fTcibqSEQ9ATWSVzA=='"2WRLPTNJW5DM2AVX52GOLZJSMI2T7JX2","da328850-a34d-4d0e-8d8a-17541a133b5c",null,FALSE,FALSE,TRUE,0);
     END LOOP;
 END $$;
 
